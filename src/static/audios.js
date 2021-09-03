763c4f7c44b94b046eb4d512710fcd1b157afceb
David <da.b@hotmail.es>
2021-09-03 12:02:53 +0200

    Nuevo tema y final

--- src/static/audios.js
+++ src/static/audios.js
@@ -2,16 +2,23 @@ const audios = [
     {
         img: process.env.PUBLIC_URL + '/assets/logo1.png',
         placeholder: process.env.PUBLIC_URL + '/assets/logo1-thumb.png',
-        src: process.env.PUBLIC_URL + '/assets/audio1.wav',
+        src: process.env.PUBLIC_URL + '/assets/audio1.mp3',
         artist: 'Da.B',
         songName: 'Drunken Monkey',
     },
     {
         img: process.env.PUBLIC_URL + '/assets/logo2.png',
         placeholder: process.env.PUBLIC_URL + '/assets/logo2-thumb.png',
-        src: process.env.PUBLIC_URL + '/assets/audio2.wav',
+        src: process.env.PUBLIC_URL + '/assets/audio2.mp3',
         artist: 'Da.B',
         songName: 'Grima en Detroit',
+    },
+    {
+        img: process.env.PUBLIC_URL + '/assets/logo3.png',
+        placeholder: process.env.PUBLIC_URL + '/assets/logo3-thumb.png',
+        src: process.env.PUBLIC_URL + '/assets/audio3.mp3',
+        artist: 'Da.B',
+        songName: 'Vibe My Ride',
     },
 ];
 
